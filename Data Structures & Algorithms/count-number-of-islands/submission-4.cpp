8dc4fb1394c3cd67be146225c70ea9ba94ea9acd
AmanBisht001 <143262778+AmanBisht001@users.noreply.github.com>
2026-09-07 13:04:17 +0530

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void solve(int row, int col, vector<vector<int>>& vis,
+           vector<vector<char>>& grid) {
+
+    int m = grid.size();
+    int n = grid[0].size();
+
+    queue<pair<int,int>> q;
+    q.push({row, col});
+    vis[row][col] = 1;  // mark immediately
+
+    int dr[] = {-1, 0, 1, 0};
+    int dc[] = {0, -1, 0, 1};
+
+    while(!q.empty()) {
+
+        int row = q.front().first;
+        int col = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++) {
+
+            int nrow = row + dr[i];
+            int ncol = col + dc[i];
+
+            if(nrow >= 0 && nrow < m &&
+               ncol >= 0 && ncol < n &&
+               grid[nrow][ncol] == '1' &&
+               vis[nrow][ncol] == 0) {
+
+                vis[nrow][ncol] = 1;  // mark before pushing
+                q.push({nrow, ncol});
+            }
+        }
+    }
+}
+
+    int numIslands(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        int count=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(!vis[i][j]&&grid[i][j]=='1'){
+                    count++;
+                    solve(i,j,vis,grid);
+                }
+            }
+        }
+        return count;
+    }
+};
