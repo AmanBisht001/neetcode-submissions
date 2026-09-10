17653691a657172501f4406f934aa0c75205e459
AmanBisht001 <143262778+AmanBisht001@users.noreply.github.com>
2026-09-10 21:29:16 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int solve(int node,int n,vector<vector<pair<int,int>>>&adj,vector<int>&vis){
+        vector<int>dis(n+1,INT_MAX);
+       
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        q.push({0,node});
+      
+        dis[node]=0;
+        while(!q.empty()){
+            auto it=q.top();
+            q.pop();
+            int node=it.second;
+            int nodeTime=it.first;
+            
+            for(auto neigh:adj[node]){
+                int NeighNode=neigh.first;
+                int temptime=neigh.second;
+                if(temptime+nodeTime<dis[NeighNode]){
+                    dis[NeighNode]=temptime+nodeTime;
+                    q.push({dis[NeighNode],NeighNode});
+                   
+                }
+            }
+        }
+        int maxi=INT_MIN;
+        for(int i=1;i<=n;i++){
+            if(dis[i]==INT_MAX){
+                return -1;
+            }
+            maxi=max(maxi,dis[i]);
+        }
+        return maxi;
+    }
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int>vis(n+1,0);
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(auto it:times){
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        int totalTime=solve(k,n,adj,vis);
+        return totalTime;
+    }
+};
